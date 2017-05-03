707800d69dadee24cc47f3ae8554357bd01f8091
Patrick Treyer <p.treyer@web.de>
2017-05-03 10:01:24 +0200

    implements BCF

--- src/picsimulator/model/befehle/pic/BCF.java
+++ src/picsimulator/model/befehle/pic/BCF.java
@@ -15,6 +15,11 @@ public class BCF extends Operation implements Executable {
 
     @Override
     public Speicher execute() {
+        int registerIndex = opcodeBits + 3;
+        String binBit = binaryString.substring(opcodeBits, registerIndex);
+        String register = binaryString.substring(registerIndex);
+        int bit = getRegisterService().binToInt(binBit);
+        memory.getFileRegister(getRegisterService().binToInt(register)).getBits()[bit].setPin(0);
         increaseProgrammCounter();
         return memory;
     }
