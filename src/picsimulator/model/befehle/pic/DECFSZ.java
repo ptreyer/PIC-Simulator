179c76fc7e25ad10f225f2f404b0a0d7d1bb9aeb
Patrick Treyer <p.treyer@web.de>
2017-05-03 14:07:01 +0200

    implements DECFSZ

--- src/picsimulator/model/befehle/pic/DECFSZ.java
+++ src/picsimulator/model/befehle/pic/DECFSZ.java
@@ -15,6 +15,29 @@ public class DECFSZ extends Operation implements Executable {
 
     @Override
     public Speicher execute() {
+        String ziel = binaryString.substring(opcodeBits, opcodeBits + 1);
+        String registerAdress = binaryString.substring(opcodeBits + 1);
+        int registerNr = getRegisterService().binToInt(registerAdress);
+
+        int aktuellerWert = memory.getFileRegister(registerNr).getIntWert();
+        int dekrementierterWert = aktuellerWert - 1;
+
+        if (Integer.parseInt(ziel) == 0) {
+            memory.setRegisterW(dekrementierterWert);
+        } else {
+            memory.getFileRegister(registerNr).setWert(dekrementierterWert);
+        }
+
+        if (memory.getFileRegister(registerNr).getIntWert() == 0) {
+            memory.getSpeicheradressen()[0].getRegister()[3].getBits()[2].setPin(1);
+        } else {
+            memory.getSpeicheradressen()[0].getRegister()[3].getBits()[2].setPin(0);
+        }
+
+        if(dekrementierterWert == 0){
+            NOP nop = new NOP(binaryString, 14, memory);
+            memory =  nop.execute();
+        }
         increaseProgrammCounter();
         return memory;
     }
