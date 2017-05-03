2f129610370d005aa67b9b455b88535652961a6d
Patrick Treyer <p.treyer@web.de>
2017-05-03 18:31:30 +0200

    implements IORWF

--- src/picsimulator/model/befehle/pic/IORWF.java
+++ src/picsimulator/model/befehle/pic/IORWF.java
@@ -27,13 +27,13 @@ public class IORWF extends Operation implements Executable {
         wRegister.setWert(memory.getRegisterW());
 
         resultRegister.getBits()[0].setPin(bits[0].getPin() | wRegister.getBits()[0].getPin());
-        resultRegister.getBits()[1].setPin(bits[0].getPin() | wRegister.getBits()[1].getPin());
-        resultRegister.getBits()[2].setPin(bits[0].getPin() | wRegister.getBits()[2].getPin());
-        resultRegister.getBits()[3].setPin(bits[0].getPin() | wRegister.getBits()[3].getPin());
-        resultRegister.getBits()[4].setPin(bits[0].getPin() | wRegister.getBits()[4].getPin());
-        resultRegister.getBits()[5].setPin(bits[0].getPin() | wRegister.getBits()[5].getPin());
-        resultRegister.getBits()[6].setPin(bits[0].getPin() | wRegister.getBits()[6].getPin());
-        resultRegister.getBits()[7].setPin(bits[0].getPin() | wRegister.getBits()[7].getPin());
+        resultRegister.getBits()[1].setPin(bits[1].getPin() | wRegister.getBits()[1].getPin());
+        resultRegister.getBits()[2].setPin(bits[2].getPin() | wRegister.getBits()[2].getPin());
+        resultRegister.getBits()[3].setPin(bits[3].getPin() | wRegister.getBits()[3].getPin());
+        resultRegister.getBits()[4].setPin(bits[4].getPin() | wRegister.getBits()[4].getPin());
+        resultRegister.getBits()[5].setPin(bits[5].getPin() | wRegister.getBits()[5].getPin());
+        resultRegister.getBits()[6].setPin(bits[6].getPin() | wRegister.getBits()[6].getPin());
+        resultRegister.getBits()[7].setPin(bits[7].getPin() | wRegister.getBits()[7].getPin());
 
         if (Integer.parseInt(ziel) == 0) {
             memory.setRegisterW(resultRegister.getIntWert());
