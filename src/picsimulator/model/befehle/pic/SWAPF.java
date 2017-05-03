3b49bc0f453686c521be0fdcccdf174c19a5a12e
Patrick Treyer <p.treyer@web.de>
2017-05-03 18:13:28 +0200

    implements SWAPF

--- src/picsimulator/model/befehle/pic/SWAPF.java
+++ src/picsimulator/model/befehle/pic/SWAPF.java
@@ -1,5 +1,7 @@
 package picsimulator.model.befehle.pic;
 
+import picsimulator.model.Bit;
+import picsimulator.model.Register;
 import picsimulator.model.Speicher;
 import picsimulator.model.befehle.Executable;
 import picsimulator.model.befehle.Operation;
@@ -15,7 +17,29 @@ public class SWAPF extends Operation implements Executable {
 
     @Override
     public Speicher execute() {
+        String ziel = binaryString.substring(opcodeBits, opcodeBits + 1);
+        String registerAdress = binaryString.substring(opcodeBits + 1);
+        int registerNr = getRegisterService().binToInt(registerAdress);
+
+        Bit[] bits = memory.getFileRegister(registerNr).getBits();
+        Register swappedRegister = new Register();
+        swappedRegister.getBits()[0] = bits[4];
+        swappedRegister.getBits()[1] = bits[5];
+        swappedRegister.getBits()[2] = bits[6];
+        swappedRegister.getBits()[3] = bits[7];
+        swappedRegister.getBits()[4] = bits[0];
+        swappedRegister.getBits()[5] = bits[1];
+        swappedRegister.getBits()[6] = bits[2];
+        swappedRegister.getBits()[7] = bits[3];
+
+        if (Integer.parseInt(ziel) == 0) {
+            memory.setRegisterW(swappedRegister.getIntWert());
+        } else {
+            memory.getFileRegister(registerNr).setWert(swappedRegister.getIntWert());
+        }
+
         increaseProgrammCounter();
         return memory;
     }
 }
+
