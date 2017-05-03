5d5052e014550d421c746c47285aed978b252674
Patrick Treyer <p.treyer@web.de>
2017-05-03 13:40:08 +0200

    implements COMF

--- src/picsimulator/model/befehle/pic/COMF.java
+++ src/picsimulator/model/befehle/pic/COMF.java
@@ -1,5 +1,6 @@
 package picsimulator.model.befehle.pic;
 
+import picsimulator.model.Bit;
 import picsimulator.model.Speicher;
 import picsimulator.model.befehle.Executable;
 import picsimulator.model.befehle.Operation;
@@ -15,6 +16,21 @@ public class COMF extends Operation implements Executable {
 
     @Override
     public Speicher execute() {
+        String registerAdress = binaryString.substring(opcodeBits);
+        int registerNr = getRegisterService().binToInt(registerAdress);
+
+        Bit[] bits = memory.getFileRegister(registerNr).getBits();
+        for (int i = 0; i < bits.length; i++) {
+            bits[i] = getRegisterService().toggleBit(bits[i]);
+        }
+        memory.getFileRegister(registerNr).setBits(bits);
+
+        if (memory.getFileRegister(registerNr).getIntWert() == 0) {
+            memory.getSpeicheradressen()[0].getRegister()[3].getBits()[2].setPin(1);
+        } else {
+            memory.getSpeicheradressen()[0].getRegister()[3].getBits()[2].setPin(0);
+        }
+
         increaseProgrammCounter();
         return memory;
     }
